9c9b14d3231a75472d330e588ddf7c94135c3ea3
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-01 20:56:36 +0300

    adding the adaptive layout in the overview and create the desktop layout of the overview

---
+++ lib/feature/overview/view/widgets/overview_desktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OverviewDesktopLayout extends StatelessWidget {
+  const OverviewDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(children: []);
+  }
+}
