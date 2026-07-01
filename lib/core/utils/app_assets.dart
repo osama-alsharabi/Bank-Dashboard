6d81fb6ba4461030a2ef7fa3ea1f77918e79806f
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-01 21:09:05 +0300

    done with generating all the assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  static const String iconsAccounts = "assets/icons/accounts.svg";
+
+  static const String iconsCreditCard = "assets/icons/credit_card.svg";
+
+  static const String iconsHome = "assets/icons/home.svg";
+
+  static const String iconsInvestments = "assets/icons/investments.svg";
+
+  static const String iconsLoans = "assets/icons/loans.svg";
+
+  static const String iconsMyPrivileges = "assets/icons/my_privileges.svg";
+
+  static const String iconsServices = "assets/icons/services.svg";
+
+  static const String iconsSettings = "assets/icons/settings.svg";
+
+  static const String iconsTransactions = "assets/icons/transactions.svg";
+
+  static const String imagesLogo = "assets/images/logo.svg";
+}
+
