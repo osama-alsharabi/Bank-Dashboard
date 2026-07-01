6d85f224100328952b356d791d3da83e06db6229
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-01 20:32:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bank_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bank_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BankDashboard());
+}
+
+class BankDashboard extends StatelessWidget {
+  const BankDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
