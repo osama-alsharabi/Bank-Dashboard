4e080bb22c486fc1590888c429cb9d0cff8fee46
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-02 19:29:40 +0300

    create recent transaction section

--- lib/feature/overview/view/overview_view.dart
+++ lib/feature/overview/view/overview_view.dart
@@ -1,5 +1,6 @@
 import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/my_card_section.dart';
+import 'package:bank_dashboard/feature/overview/view/widgets/recent_transaction_section.dart';
 import 'package:flutter/material.dart';
 
 class OverviewView extends StatelessWidget {
@@ -13,12 +14,7 @@ class OverviewView extends StatelessWidget {
         CustomAppBar(title: "Overview"),
         Padding(
           padding: EdgeInsets.all(16),
-          child: Row(
-            children: [
-              MyCardSection(),
-              Expanded(child: Column()),
-            ],
-          ),
+          child: Row(children: [MyCardSection(), RecentTransactionSection()]),
         ),
       ],
     );
---
+++ lib/feature/overview/view/widgets/recent_transaction_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecentTransactionSection extends StatelessWidget {
+  const RecentTransactionSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(child: Column());
+  }
+}
