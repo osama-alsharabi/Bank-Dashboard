7bb6a884dac233eb02bab3eb2bebf1dd1c2705aa
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-03 20:27:22 +0300

    create the weekly activity section

--- lib/feature/overview/view/overview_view.dart
+++ lib/feature/overview/view/overview_view.dart
@@ -1,6 +1,7 @@
 import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/my_card_section.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/recent_transaction_section.dart';
+import 'package:bank_dashboard/feature/overview/view/widgets/weekly_activity_section.dart';
 import 'package:flutter/material.dart';
 
 class OverviewView extends StatelessWidget {
@@ -12,15 +13,28 @@ class OverviewView extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         CustomAppBar(title: "Overview"),
-        Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: [
-              Expanded(flex: 2, child: MyCardSection()),
-              SizedBox(width: 25),
-              Expanded(child: RecentTransactionSection()),
-            ],
-          ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(flex: 2, child: MyCardSection()),
+                  SizedBox(width: 25),
+                  Expanded(child: RecentTransactionSection()),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.all(16),
+              child: Row(
+                children: [
+                  Expanded(flex: 2, child: WeeklyActivitySection()),
+                  Expanded(flex: 1, child: SizedBox()),
+                ],
+              ),
+            ),
+          ],
         ),
       ],
     );
---
+++ lib/feature/overview/view/widgets/weekly_activity_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeeklyActivitySection extends StatelessWidget {
+  const WeeklyActivitySection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
