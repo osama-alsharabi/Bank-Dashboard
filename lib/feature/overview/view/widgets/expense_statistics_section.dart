b5fefcbe8f0bcb1f810b23bdc844c421963f26e0
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-05 22:30:31 +0300

    create the expense statistics title header

--- lib/feature/overview/view/widgets/expense_statistics_section.dart
+++ lib/feature/overview/view/widgets/expense_statistics_section.dart
@@ -1,3 +1,5 @@
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseStatisticsSection extends StatelessWidget {
@@ -5,6 +7,16 @@ class ExpenseStatisticsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Column(
+      children: [
+        Text(
+          'Expense Statistics',
+          style: context.textStyle.textStyleSemibold22.copyWith(
+            color: AppColor.secondaryColor,
+          ),
+        ),
+        
+      ],
+    );
   }
 }
