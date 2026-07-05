d878ef62912fc8a5c8871db9520fdfd74824e119
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-05 23:21:48 +0300

    create the third section of the overview and also create the quick transfer section

--- lib/feature/overview/view/overview_view.dart
+++ lib/feature/overview/view/overview_view.dart
@@ -1,6 +1,7 @@
 import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/expense_statistics_section.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/my_card_section.dart';
+import 'package:bank_dashboard/feature/overview/view/widgets/quick_transfer_section.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/recent_transaction_section.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/weekly_activity_section.dart';
 import 'package:flutter/material.dart';
@@ -40,6 +41,16 @@ class OverviewView extends StatelessWidget {
                     Expanded(flex: 2, child: WeeklyActivitySection()),
                     SizedBox(width: 25),
                     Expanded(flex: 1, child: ExpenseStatisticsSection()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.all(16),
+                child: Row(
+                  children: [
+                    Expanded(flex: 1, child: QuickTransferSection()),
+                    SizedBox(width: 25),
+                    Expanded(flex: 3, child: SizedBox()),
                   ],
                 ),
               ),
---
+++ lib/feature/overview/view/widgets/quick_transfer_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuickTransferSection extends StatelessWidget {
+  const QuickTransferSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
