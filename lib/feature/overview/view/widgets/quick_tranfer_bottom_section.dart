babcfc2262bc476825f20cb657dd9ed396cb7731
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-06 00:17:07 +0300

    done with bottom section of the ui of quick transfer section

--- lib/feature/overview/view/widgets/quick_tranfer_bottom_section.dart
+++ lib/feature/overview/view/widgets/quick_tranfer_bottom_section.dart
@@ -9,12 +9,56 @@ class QuickTransferBottomSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
+        const SizedBox(width: 15),
         Text(
           "Write Amount",
           style: context.textStyle.textStyleRegular15.copyWith(
             color: AppColor.customGreyColor,
           ),
         ),
+        const SizedBox(width: 27),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  decoration: const BoxDecoration(
+                    color: AppColor.scaffoldBackgroundColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      topLeft: Radius.circular(50),
+                    ),
+                  ),
+                  child: const Center(child: Text("525.50")),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  decoration: const BoxDecoration(
+                    color: AppColor.primaryColor,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Send", style: TextStyle(color: Colors.white)),
+                        Icon(Icons.send, color: Colors.white),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 16),
       ],
     );
   }
