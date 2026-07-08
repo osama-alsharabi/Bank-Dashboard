6ded54e51035edb170b05aa002606e755ef4d02f
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-08 14:07:30 +0300

    create the balance history title

--- lib/feature/overview/view/widgets/balance_history_section.dart
+++ lib/feature/overview/view/widgets/balance_history_section.dart
@@ -1,3 +1,5 @@
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class BalanceHistorySection extends StatelessWidget {
@@ -5,6 +7,15 @@ class BalanceHistorySection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Column(
+      children: [
+        Text(
+          "Balance History",
+          style: context.textStyle.textStyleSemibold22.copyWith(
+            color: AppColor.secondaryColor,
+          ),
+        ),
+      ],
+    );
   }
 }
