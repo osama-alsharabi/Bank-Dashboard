0a39a6c8cf304125e99274c2b4a086104467eff2
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-08 16:06:44 +0300

    create the My Expense section

--- lib/feature/transactions/view/transactions_view.dart
+++ lib/feature/transactions/view/transactions_view.dart
@@ -1,5 +1,6 @@
 import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:bank_dashboard/feature/overview/view/widgets/my_card_section.dart';
+import 'package:bank_dashboard/feature/transactions/view/widgets/my_expense_section.dart';
 import 'package:flutter/material.dart';
 
 class TransactionsView extends StatelessWidget {
@@ -26,7 +27,7 @@ class TransactionsView extends StatelessWidget {
                   children: [
                     Expanded(flex: 2, child: MyCardSection()),
                     SizedBox(width: 25),
-                    Expanded(child: SizedBox()),
+                    Expanded(child: MyExpenseSection()),
                   ],
                 ),
               ),
---
+++ lib/feature/transactions/view/widgets/my_expense_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyExpenseSection extends StatelessWidget {
+  const MyExpenseSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
