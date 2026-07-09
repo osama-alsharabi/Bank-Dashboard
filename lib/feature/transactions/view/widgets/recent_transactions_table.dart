6f9c8686f6490ce34f747defeda1104fa6174cc2
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-09 18:22:30 +0300

    done with creating the table title

--- lib/feature/transactions/view/widgets/recent_transactions_table.dart
+++ lib/feature/transactions/view/widgets/recent_transactions_table.dart
@@ -1,15 +1,47 @@
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
-class RecentTransactionsTable extends StatelessWidget {
+class RecentTransactionsTable extends StatefulWidget {
   const RecentTransactionsTable({super.key});
 
+  @override
+  State<RecentTransactionsTable> createState() =>
+      _RecentTransactionsTableState();
+}
+
+class _RecentTransactionsTableState extends State<RecentTransactionsTable> {
+  static const List<String> tableTitle = [
+    "Description",
+    "Transaction ID",
+    "Type",
+    "Card",
+    "Date",
+    "Amount",
+    "Receipt",
+  ];
   @override
   Widget build(BuildContext context) {
     return Table(
       border: const TableBorder(
         horizontalInside: BorderSide(width: 1, color: Color(0xff718EBF)),
       ),
-      children: [],
+      children: [
+        TableRow(
+          children: tableTitle
+              .map(
+                (e) => Padding(
+                  padding: const EdgeInsetsGeometry.symmetric(vertical: 10),
+                  child: Text(
+                    e,
+                    style: context.textStyle.textStyleSemibold16.copyWith(
+                      color: const Color(0xff718EBF),
+                    ),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ],
     );
   }
 }
