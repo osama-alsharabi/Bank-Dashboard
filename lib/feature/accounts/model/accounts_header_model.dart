a558b24eb3b9a1a67926bd29b1a5e0786c4fdf16
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-11 17:31:21 +0300

    create the accounts header model

---
+++ lib/feature/accounts/model/accounts_header_model.dart
@@ -0,0 +1,11 @@
+class AccountsHeaderModel {
+  final String title;
+  final String icon;
+  final double amount;
+
+  AccountsHeaderModel({
+    required this.title,
+    required this.icon,
+    required this.amount,
+  });
+}
