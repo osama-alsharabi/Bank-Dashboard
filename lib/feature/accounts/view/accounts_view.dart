75cf83295d06e9fe096e02c394fcabd0da2cd734
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-11 16:59:53 +0300

    create the accounts view

---
+++ lib/feature/accounts/view/accounts_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountsView extends StatelessWidget {
+  const AccountsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
