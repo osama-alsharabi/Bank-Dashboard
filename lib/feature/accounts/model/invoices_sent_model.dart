604cfe417fcd526c453fc89d0c0af7fa389a5bdb
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-15 10:44:52 +0300

    create the model of the invoices sent

---
+++ lib/feature/accounts/model/invoices_sent_model.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class InvoicesSentModel {
+  final String icon;
+  final String title;
+  final String amount;
+  final String time;
+  final Color color;
+
+  InvoicesSentModel({
+    required this.icon,
+    required this.title,
+    required this.amount,
+    required this.time,
+    required this.color,
+  });
+}
