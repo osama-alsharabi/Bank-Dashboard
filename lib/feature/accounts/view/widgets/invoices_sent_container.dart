fb9458194fa1f4b0f5f0ae9910cd98f272e3b199
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-15 10:37:34 +0300

    create the header of the item of invoices sent

--- lib/feature/accounts/view/widgets/invoices_sent_container.dart
+++ lib/feature/accounts/view/widgets/invoices_sent_container.dart
@@ -1,4 +1,6 @@
+import 'package:bank_dashboard/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class InvoicesSentContainer extends StatelessWidget {
   const InvoicesSentContainer({super.key});
@@ -9,6 +11,18 @@ class InvoicesSentContainer extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(25),
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: const Color(0xffE5E5E5),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: SvgPicture.asset(AppAssets.iconsAccountExpense),
+          ),
+        ],
       ),
     );
   }
