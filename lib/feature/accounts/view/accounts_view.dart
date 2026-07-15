feae6b732690d71b49cb2bbe67f54c0004545166
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-15 10:00:26 +0300

    create the Debit & Credit Overview Section

--- lib/feature/accounts/view/accounts_view.dart
+++ lib/feature/accounts/view/accounts_view.dart
@@ -1,6 +1,7 @@
 import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:bank_dashboard/feature/accounts/view/widgets/account_my_card.dart';
 import 'package:bank_dashboard/feature/accounts/view/widgets/accounts_header.dart';
+import 'package:bank_dashboard/feature/accounts/view/widgets/debit_and_credit_overview_section.dart';
 import 'package:bank_dashboard/feature/accounts/view/widgets/last_transaction_section.dart';
 import 'package:flutter/material.dart';
 
@@ -38,6 +39,16 @@ class AccountsView extends StatelessWidget {
                     Expanded(flex: 2, child: LastTransactionSection()),
                     SizedBox(width: 30),
                     Expanded(child: AccountMyCard()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  children: [
+                    Expanded(flex: 2, child: DebitAndCreditOverviewSection()),
+                    SizedBox(width: 30),
+                    Expanded(child: SizedBox()),
                   ],
                 ),
               ),
---
+++ lib/feature/accounts/view/widgets/debit_and_credit_overview_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DebitAndCreditOverviewSection extends StatelessWidget {
+  const DebitAndCreditOverviewSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
