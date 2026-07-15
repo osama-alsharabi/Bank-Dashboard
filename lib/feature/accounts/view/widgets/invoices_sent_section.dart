d808cb5a0102622f5f215a7dc7790d0054e113b9
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-15 10:32:16 +0300

    create the Invoices Send Header

--- lib/feature/accounts/view/widgets/invoices_sent_section.dart
+++ lib/feature/accounts/view/widgets/invoices_sent_section.dart
@@ -1,3 +1,5 @@
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class InvoicesSentSection extends StatelessWidget {
@@ -5,6 +7,17 @@ class InvoicesSentSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Invoices Sent",
+          style: context.textStyle.textStyleSemibold22.copyWith(
+            color: AppColor.secondaryColor,
+          ),
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
   }
 }
