66300bf41c043bc127e03a4b9fbe932807292e3d
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-16 22:07:44 +0300

    create teh investements header  model

---
+++ lib/feature/investments/model/investments_header_model.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class InvestmentsHeaderModel {
+  final String title;
+  final String icon;
+  final double amount;
+  final Color color;
+
+  const InvestmentsHeaderModel({
+    required this.title,
+    required this.icon,
+    required this.amount,
+    required this.color,
+  });
+}
