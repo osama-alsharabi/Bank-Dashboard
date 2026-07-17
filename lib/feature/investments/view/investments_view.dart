778eb0c226dd96f4fa135f4e433e929997d98fd7
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-17 17:05:29 +0300

    create the yearly total investment section

--- lib/feature/investments/view/investments_view.dart
+++ lib/feature/investments/view/investments_view.dart
@@ -1,5 +1,6 @@
 import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:bank_dashboard/feature/investments/view/widgets/investments_header.dart';
+import 'package:bank_dashboard/feature/investments/view/widgets/yearly_total_investment_section.dart';
 import 'package:flutter/material.dart';
 
 class InvestmentsView extends StatelessWidget {
@@ -23,6 +24,10 @@ class InvestmentsView extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                 child: InvsetmentsHeader(),
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+                child: YearlyTotalInvestmentSection(),
               ),
             ],
           ),
---
+++ lib/feature/investments/view/widgets/yearly_total_investment_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YearlyTotalInvestmentSection extends StatelessWidget {
+  const YearlyTotalInvestmentSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
