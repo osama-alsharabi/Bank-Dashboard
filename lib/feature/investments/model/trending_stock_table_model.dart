82e96c904fd9a0424135ec0fa92b2953926933ac
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-18 22:39:33 +0300

    create the trending stock table model

---
+++ lib/feature/investments/model/trending_stock_table_model.dart
@@ -0,0 +1,13 @@
+class TrendingStockTableModel {
+  final int sLNo;
+  final String name;
+  final double price;
+  final double returnPrecent;
+
+  TrendingStockTableModel({
+    required this.sLNo,
+    required this.name,
+    required this.price,
+    required this.returnPrecent,
+  });
+}
