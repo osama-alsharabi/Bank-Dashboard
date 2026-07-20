5b445eeed58461842b4fd8ad7ea81a0b72820df1
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-21 01:12:24 +0300

    adding the app bar of the credit cards feature

--- lib/feature/credit_cards/view/widgets/credit_cards_body.dart
+++ lib/feature/credit_cards/view/widgets/credit_cards_body.dart
@@ -1,3 +1,4 @@
+import 'package:bank_dashboard/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class CreditCardsBody extends StatelessWidget {
@@ -5,6 +6,17 @@ class CreditCardsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return const CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          floating: false,
+          expandedHeight: 80,
+          collapsedHeight: 80,
+          toolbarHeight: 80,
+          flexibleSpace: CustomAppBar(title: "Credit Cards"),
+        ),
+      ],
+    );
   }
 }
