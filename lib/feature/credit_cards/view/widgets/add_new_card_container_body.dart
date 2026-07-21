0a9fffb0b0384468b98a5c76dbe7fcac609df30f
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-21 21:25:59 +0300

    adding the text note of the new card container

--- lib/feature/credit_cards/view/widgets/add_new_card_container_body.dart
+++ lib/feature/credit_cards/view/widgets/add_new_card_container_body.dart
@@ -1,3 +1,5 @@
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class AddNewCardContainerBody extends StatelessWidget {
@@ -5,6 +7,15 @@ class AddNewCardContainerBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      children: [
+        Text(
+          "Credit Card generally means a plastic card issued by Scheduled Commercial Banks assigned to a Cardholder, with a credit limit, that can be used to purchase goods and services on credit or obtain cash advances.",
+          style: context.textStyle.textStyleRegular16.copyWith(
+            color: AppColor.customSecondaryGreyColor,
+          ),
+        ),
+      ],
+    );
   }
 }
