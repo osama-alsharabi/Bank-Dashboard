0952d286b86316f4ed8f0a2f3d7cf8140ba3fcb9
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-21 04:24:13 +0300

    done with the card list item UI

--- lib/feature/credit_cards/view/widgets/card_list_item.dart
+++ lib/feature/credit_cards/view/widgets/card_list_item.dart
@@ -1,4 +1,8 @@
+import 'package:bank_dashboard/core/utils/app_assets.dart';
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CardListItem extends StatelessWidget {
   const CardListItem({super.key});
@@ -6,11 +10,164 @@ class CardListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 23, horizontal: 16),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
       ),
-      child: const Row(children: []),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(17),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: SvgPicture.asset(AppAssets.iconsAccountExpense),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Card Type",
+                    style: context.textStyle.textStyleSemibold16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Secondary",
+                    style: context.textStyle.textStyleRegular15.copyWith(
+                      color: AppColor.customSecondaryGreyColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Bank",
+                    style: context.textStyle.textStyleSemibold16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Secondary",
+                    style: context.textStyle.textStyleRegular15.copyWith(
+                      color: AppColor.customSecondaryGreyColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Card Number",
+                    style: context.textStyle.textStyleSemibold16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "**** **** 5600",
+                    style: context.textStyle.textStyleRegular15.copyWith(
+                      color: AppColor.customSecondaryGreyColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Bank",
+                    style: context.textStyle.textStyleSemibold16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+
+                  child: Text(
+                    "Secondary",
+                    style: context.textStyle.textStyleRegular15.copyWith(
+                      color: AppColor.customSecondaryGreyColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+
+                  child: Text(
+                    "Namain Card",
+                    style: context.textStyle.textStyleSemibold16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+
+                  child: Text(
+                    "William",
+                    style: context.textStyle.textStyleRegular15.copyWith(
+                      color: AppColor.customSecondaryGreyColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                "View Details",
+                style: context.textStyle.textStyleSemibold16.copyWith(
+                  color: AppColor.primaryColor,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+        ],
+      ),
     );
   }
 }
