d876052e8fa76e2a3052bf15febcad9b548753d7
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-21 21:16:52 +0300

    adding of the title of the add new card section

--- lib/feature/credit_cards/view/widgets/add_new_card_section.dart
+++ lib/feature/credit_cards/view/widgets/add_new_card_section.dart
@@ -1,3 +1,5 @@
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class AddNewCardSection extends StatelessWidget {
@@ -5,6 +7,17 @@ class AddNewCardSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Add New Card",
+          style: context.textStyle.textStyleSemibold22.copyWith(
+            color: AppColor.secondaryColor,
+          ),
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
   }
 }
