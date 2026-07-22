4747949671b7f513ba33758835ba76206fcac6b7
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-23 01:54:16 +0300

    create teh loans header model

---
+++ lib/feature/loans/model/loans_header_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoansHeaderModel {
+  final String title;
+  final String icon;
+  final Color color;
+  final double price;
+
+  const LoansHeaderModel({
+    required this.title,
+    required this.icon,
+    required this.color,
+    required this.price,
+  });
+}
