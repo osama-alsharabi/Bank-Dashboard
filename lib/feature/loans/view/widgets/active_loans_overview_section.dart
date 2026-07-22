b4ac8d9209eb45f54fbd5c9c2998519f9c5f6d27
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-23 02:38:04 +0300

    adding the title of active loans overview in the section

--- lib/feature/loans/view/widgets/active_loans_overview_section.dart
+++ lib/feature/loans/view/widgets/active_loans_overview_section.dart
@@ -1,3 +1,5 @@
+import 'package:bank_dashboard/core/utils/app_color.dart';
+import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class ActiveLoansOverviewSection extends StatelessWidget {
@@ -5,6 +7,16 @@ class ActiveLoansOverviewSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Active Loans Overview",
+          style: context.textStyle.textStyleSemibold22.copyWith(
+            color: AppColor.secondaryColor,
+          ),
+        ),
+      ],
+    );
   }
 }
