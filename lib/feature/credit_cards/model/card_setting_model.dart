74459d1a7b0b69c5d48a19aef633d2a2b1cd567a
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-23 01:07:56 +0300

    create the model of card setting

---
+++ lib/feature/credit_cards/model/card_setting_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardSettingModel {
+  final String icon;
+  final Color color;
+  final String title;
+  final String description;
+
+  const CardSettingModel({
+    required this.icon,
+    required this.color,
+    required this.title,
+    required this.description,
+  });
+}
