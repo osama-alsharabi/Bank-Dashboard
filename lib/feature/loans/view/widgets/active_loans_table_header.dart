92dce371309ac3adb0cec79f1c920e9b48396a31
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-07-23 07:11:39 +0300

    refactoring the code of active loans table header

--- lib/feature/loans/view/widgets/active_loans_table_header.dart
+++ lib/feature/loans/view/widgets/active_loans_table_header.dart
@@ -2,72 +2,29 @@ import 'package:bank_dashboard/core/utils/app_color.dart';
 import 'package:bank_dashboard/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 
+const List<String> tableHeader = [
+  "SL No",
+  "Loan Money",
+  "Left to repay",
+  "Duration",
+  "Interest rate",
+  "Installment",
+  "Repay",
+];
 TableRow activeLoansTableHeader(BuildContext context) {
   return TableRow(
-    children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "SL No",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
+    children: tableHeader
+        .map(
+          (e) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              e,
+              style: context.textStyle.textStyleSemibold16.copyWith(
+                color: AppColor.customSecondaryGreyColor,
+              ),
+            ),
           ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "Loan Money",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "Left to repay",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "Duration",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "Interest rate",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "Installment",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text(
-          "Repay",
-          style: context.textStyle.textStyleSemibold16.copyWith(
-            color: AppColor.customSecondaryGreyColor,
-          ),
-        ),
-      ),
-    ],
+        )
+        .toList(),
   );
 }
